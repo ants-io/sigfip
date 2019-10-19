20fdd5dd98ea8bb447adb42e8416e776c4bf3047
Dieynab Diatta <seydimomogueye@gmail.com>
2019-10-19 01:55:06 +0000

    Initial commit, set user models.

---
+++ sigfip/users/behaviours.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.db.models import (CharField, DateTimeField, TextField, )
+from django.utils.translation import ugettext_lazy as _
+
+from model_utils.models import TimeStampedModel
+
+
+class NameField(models.Model):
+    """
+    This abstract model will be inherited by all model which have name and description fields.
+    """
+
+    name = CharField(_("Nom"), max_length=20)
+    description = TextField(_("Description"))
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}: {self.description}'
+
+
+class TimeStampedField(models.Model):
+    """
+    This abstract model allow to add timestamped fields to every models who need them.
+    """
+
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['created_at']
---
+++ sigfip/users/models.py
@@ -0,0 +1,167 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import (
+    CharField,
+    DateField,
+    FileField,
+    FloatField,
+    ForeignKey,
+    IntegerField,
+    ManyToManyField,
+    OneToOneField,
+    TextField,
+)
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from model_utils.models import StatusModel
+from model_utils import Choices
+
+from .behaviours import (NameField, TimeStampedField)
+
+
+class User(AbstractUser):
+    first_name = CharField(_("Prénom"), blank=True, null=True, max_length=255)
+    last_name = CharField(_("Nom"), blank=True, null=True, max_length=255)
+    birth_date = DateField(_("Date de naissance"), blank=True, null=True)
+    birth_place = CharField(_("Lieu de naissance"), blank=True, null=True, max_length=255)
+    sex = IntegerField(_("Sexe"), choices=(
+        (0, _("Féminin")),
+        (1, _("Masculin")),
+    ), default=1)
+    registration_number = CharField(_("Matricule"), blank=True, null=True, max_length=100)
+    registration_date = DateField(_("Date d'entrée"), blank=True, null=True)
+    cni = CharField(_("Numéro Carte d'identité"), blank=True, null=True, max_length=100)
+
+    def full_name(self):
+        return f'{self.name} {self.last_name}'
+
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Salary(TimeStampedField):
+    """
+    Model that track all user's salary evolution.
+    """
+
+    user = ForeignKey(User, on_delete=models.CASCADE)
+    kind = IntegerField(_("Type"), choices=(
+        (0, _("Brute")),
+        (1, _("NET")),
+    ))
+    amount = FloatField(_("Montant"))
+    change_at = DateField(_("Date mise à jour"), blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'salaries'
+
+    def __str__(self):
+        return f'{self.user.full_name}: {self.amount * 12} FCFA Annual.'
+
+
+class Corps(NameField):
+    """
+    Model that store all existing Corps in System.
+    """
+    
+    class Meta:
+        verbose_name_plural = 'corps'
+
+
+class Ministry(NameField):
+    """
+    Model that store all existing Corps in System.
+    """
+    
+    class Meta:
+        verbose_name_plural = 'ministers'
+
+
+class PayingOrg(NameField, TimeStampedField):
+    """
+    Model that store all existing paying org in System, 
+    where the related user get paid from.
+    """
+    
+    class Meta:
+        verbose_name_plural = 'paying_orgs'
+
+
+class AdditionalInformation(TimeStampedField):
+    """
+    Additionnal informations for user.
+    """
+    user = OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True)
+    address = TextField(_("Adresse"))
+    postal_box = CharField(_("Boite postale"), max_length=20)
+    phone = CharField(_("Mobile / Téléphone"), max_length=20)
+    corps = ForeignKey(
+        Corps, verbose_name=_("Corps"), on_delete=models.CASCADE)
+    ministry = ForeignKey(
+        Ministry, verbose_name=_("Ministère"), on_delete=models.CASCADE)
+    paying_org = ForeignKey(
+        PayingOrg,
+        verbose_name=_("Organisme payeur"),
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.full_name
+    
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"username": self.username})
+
+
+class DocumentCategory(NameField, TimeStampedField):
+    """
+    All Document categories. (CNI, Passport, 'Birth act', ...)
+    """
+
+    class Meta:
+        verbose_name_plural = 'document_categories'
+
+
+class RequestCategory(NameField, TimeStampedField):
+    """
+    Request categories and all required document for each category.
+    """
+    documents = ManyToManyField(DocumentCategory)
+
+    class Meta:
+        verbose_name_plural = 'request_categories'
+
+
+class Request(TimeStampedField):
+    """
+    Application for user to have a loan.
+    """
+    amount = FloatField(_("Montant de la demande"))
+    date = DateField(_("Date de dépôt de la demande"))
+    post_reference = CharField(_("Référence courier"), max_length=20)
+    category = ForeignKey(
+        RequestCategory,
+        verbose_name=_("Catégorie de la demande"),
+        on_delete=models.CASCADE)
+    observations = TextField(_("Observations"), blank=True)
+    status = Choices('accepted', 'archived', 'frozen', 'rejected',)
+
+    class Meta:
+        ordering = ['created_at']
+
+
+class Document(TimeStampedField):
+    """
+    Document given by user to apply for a loan.
+    """
+    physical_document = FileField(_("Document"))
+    reference = ForeignKey(
+        DocumentCategory,
+        verbose_name=_("Document de reference"),
+        on_delete=models.CASCADE)
+    request = ForeignKey(
+        Request,
+        verbose_name=_("Demande correspondante"),
+        on_delete=models.CASCADE)
